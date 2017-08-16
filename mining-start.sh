d7a61ff868ba68f15625e32b7609611c6463fc24
Cristiano Caldas <eu@cccaldas.com>
2017-08-16 15:11:21 -0300

    eth

--- mining-start.sh
+++ mining-start.sh
@@ -4,10 +4,10 @@
 #minergate-cli -user eu@cccaldas.com -bcn&
 
 #mining ETH - ethereum
-#ethminer -C -F http://eth.pool.minergate.com:55751/eu@cccaldas.com --disable-submit-hashrate&
+ethminer -C -F http://eth.pool.minergate.com:55751/eu@cccaldas.com --disable-submit-hashrate&
 
 #mining DSH - dashcoin
 #minergate-cli -user eu@cccaldas.com -dsh&
 
 #mining XMR - monero
-minergate-cli -user eu@cccaldas.com -xmr&
+#minergate-cli -user eu@cccaldas.com -xmr&
