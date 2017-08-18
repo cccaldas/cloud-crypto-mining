3b0ee2b4765627ed79507edbbf270cf67d482e19
Cristiano Caldas <eu@cccaldas.com>
2017-08-18 08:53:42 -0300

    bcn

--- mining-start.sh
+++ mining-start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #mining BCN - bytecoin
-#minergate-cli -user eu@cccaldas.com -bcn&
+minergate-cli -user eu@cccaldas.com -bcn&
 
 #mining ETH - ethereum
-ethminer -C -F http://eth.pool.minergate.com:55751/eu@cccaldas.com --disable-submit-hashrate&
+#ethminer -C -F http://eth.pool.minergate.com:55751/eu@cccaldas.com --disable-submit-hashrate&
 
 #mining DSH - dashcoin
 #minergate-cli -user eu@cccaldas.com -dsh&
