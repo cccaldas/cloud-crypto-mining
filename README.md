3b2ca0c2bf8bf4d697e89f7719d06a9dabb4a0f9
Cristiano Caldas <eu@cccaldas.com>
2017-08-26 00:08:53 -0300

    weekend

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cloud-crypto-mining
+
+
+#CPU miner
+https://github.com/pooler/cpuminer
+
+sudo wget https://github.com/cccaldas/cloud-crypto-mining/raw/master/bin/minerd-linux -O /usr/bin/minerd && sudo chmod a+x /usr/bin/minerd
--- mining-stop.sh
+++ mining-stop.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-killall minergate-cli
-killall ethminer
-killall minerd
+DAYOFWEEK=$(date +"%u")
+echo DAYOFWEEK: $DAYOFWEEK
+
+#DAYOFWEEK=4
+
+if [ "${DAYOFWEEK}" -lt 6 ];  
+then #WEEKDAY
+	
+	killall minergate-cli;
+	killall ethminer;
+	killall minerd;
+	
+else #WEEKEND
+	echo "WEEKEND";
+fi
+
