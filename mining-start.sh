84946db72bb1f4e50ed4c73efee64a0b62d2e97d
Cristiano Caldas <eu@cccaldas.com>
2017-09-08 12:09:01 -0300

    xmr

--- mining-start.sh
+++ mining-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #mining BCN - bytecoin
-minergate-cli -user eu@cccaldas.com -bcn&
+#minergate-cli -user eu@cccaldas.com -bcn&
 
 #mining ETH - ethereum
 #ethminer -C -F http://eth.pool.minergate.com:55751/eu@cccaldas.com --disable-submit-hashrate&
@@ -10,7 +10,7 @@ minergate-cli -user eu@cccaldas.com -bcn&
 #minergate-cli -user eu@cccaldas.com -dsh&
 
 #mining XMR - monero
-#minergate-cli -user eu@cccaldas.com -xmr&
+minergate-cli -user eu@cccaldas.com -xmr&
 
 #mining LTC
 #minerd -s 1 -o stratum+tcp://ltc.pool.minergate.com:3336 -u eu@cccaldas.com&
